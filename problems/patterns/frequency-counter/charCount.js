dd943b0d4e3d2b40540b7f0fdd521f548e5907b7
JBeCast <jbcsound@gmail.com>
2019-01-13 15:56:30 +0100

    Restructured project.

---
+++ problems/patterns/frequency-counter/charCount.js
@@ -0,0 +1,10 @@
+function charCount(str) {
+    const frequency = {};
+
+    for (let char of str) {
+        const c = char.toLowerCase();
+        /[a-z0-9]/.test(c) && frequency[c] ? frequency[c]++ : frequency[c] = 1;
+    }
+
+    return frequency;
+}

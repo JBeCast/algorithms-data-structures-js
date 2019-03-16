2babf16b5cd397e218eb1c04cb7338264b25dce1
JBeCast <jbcsound@gmail.com>
2019-03-16 22:16:25 +0100

    Bugfixing and addition of uncommented version of singly linked list. Moved every comment inline.

--- data-structures/doubly-linked-list/DoublyLinkedList-commented.js
+++ data-structures/doubly-linked-list/DoublyLinkedList-commented.js
@@ -1,173 +1,118 @@
+// Implementation of a doubly linked list and all of its methods
+
 // A node in the doubly linked list, using a factory function instead of a class
 const Node = val => ({
-  val,
-  next: null,
-  prev: null,
+  val, // Value of the node
+  prev: null, // Reference to the previous node in the list
+  next: null, // Reference to the next node in the list
 });
 
 class DoublyLinkedList {
   constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
+    // Reference to the head (first node in the list)
+    this.head = null; // Reference to the head (first node in the list)
+    this.tail = null; // Reference to the tail (last node in the list)
+    this.length = 0; // Length of the list
   }
 
   push(val) { // Insert a node at the end of the list
-    // Create the new node
-    const node = Node(val);
-    // If there were no nodes in the list, set the node as the head and tail
-    if (!this.length) this.head = this.tail = node;
+    const node = Node(val); // Create the new node
+    if (!this.length) this.head = this.tail = node; // If there were no nodes in the list, set the node as the head and tail
     else {
-      // Point the prev link of the new node to the previous tail
-      node.prev = this.tail;
-      // Point the previous tail's next to the new node
-      this.tail.next = node;
-      // Set the new node as tail
-      this.tail = node;
+      node.prev = this.tail; // Point the prev link of the new node to the previous tail
+      this.tail.next = node; // Point the previous tail's next to the new node
+      this.tail = node; // Set the new node as tail
     }
-    // Increase list length
-    ++this.length;
-    // Return a reference to the list, so we can chain methods if needed
-    return this;
+    this.length++; // Increase list length
+    return this; // Return a reference to the list, so we can chain methods if needed
   }
 
   pop() { // Remove and return the last node
-    // If there are no nodes in the list, return null
-    if (!this.length) return null;
-    // Keep a reference to the node to be popped (previous tail)
-    const oldTail = this.tail;
-    // Point the tail to the previous node (will be null if there wasn't any node)
-    this.tail = oldTail.prev;
-    // Isolate the popped node
-    oldTail.prev = oldTail.next = null;
-    // Decrease the length of the list
-    --this.length;
-    // If the list has now no length, set the head to null as well
-    if (!this.length) this.head = null;
-    // Else set only the tail's next to null
-    else this.tail.next = null;
-    // Return the popped node
-    return oldTail;
+    if (!this.length) return null; // If there are no nodes in the list, return null
+    const oldTail = this.tail; // Keep a reference to the node to be popped (previous tail)
+    this.tail = oldTail.prev; // Point the tail to the previous node (will be null if there wasn't any node)
+    oldTail.prev = oldTail.next = null; // Isolate the popped node
+    this.length--; // Subtract 1 to the length of the list
+    if (!this.length) this.head = null; // If the list has now no length, set the head to null as well
+    else this.tail.next = null; // Else set only the tail's next to null
+    return oldTail; // Return the popped node
   }
 
   shift() { // Remove and return element from the beginning of the list
-    if (!this.length) return null;
-    // Keep a reference the node to be removed (previous head)
-    const oldHead = this.head;
-    // Point the head to the next node (will be null if there wasn't any node)
-    this.head = oldHead.next;
-    // Isolate the popped node
-    oldHead.prev = oldHead.next = null;
-    // Decrease the length of the list
-    --this.length;
-    // If the list has now no length, set the tail to null as well
-    // Else set only the head's next to null
-    if (!this.length) this.tail = null;
-    else this.head.prev = null;
-    // Return the node we have removed
-    return oldHead;
+    if (!this.length) return null; // If there are no nodes in the list, return null
+    const oldHead = this.head; // Keep a reference the node to be removed (previous head)
+    this.head = oldHead.next; // Point the head to the next node (will be null if there wasn't any node)
+    oldHead.prev = oldHead.next = null; // Isolate the popped node
+    this.length--; // Decrease the length of the list
+    if (!this.length) this.tail = null; // If the list has now no length, set the tail to null as well
+    else this.head.prev = null; // Else set only the head's next to null
+    return oldHead; // Return the node we have removed
   }
 
   unshift(val) { // Insert a new node at the beginning of the list
-    // Create the new node
-    const newHead = Node(val);
-    // If there were no nodes in the list, set the node as the head and tail
-    if (!this.length) this.head = this.tail = newHead;
+    const newHead = Node(val); // Create the new node
+    if (!this.length) this.head = this.tail = newHead; // If there were no nodes in the list, set the node as the head and tail
     else {
-      // Point the next link of the new node to the previous head
-      newHead.next = this.head;
-      // Point the previous head's prev to the new node
-      this.head.prev = newHead;
-      // Set the new node as head
-      this.head = newHead;
+      newHead.next = this.head; // Point the next link of the new node to the previous head
+      this.head.prev = newHead; // Point the previous head's prev to the new node
+      this.head = newHead; // Set the new node as head
     }
-    // Increase list length
-    ++this.length;
-    // Return the list;
-    return this;
+    this.length++; // Add 1 to the list length
+    return this; // Return a reference to the list
   }
 
   get(index) { // get the node at the given index (1-based)
-    // Edge cases
-    if (!this.length || index < 1 || index > this.length) return null;
-    // Depending on the index, we'll traverse the list forward or backwards
-    else return index <= this.length / 2 ?
-      this.getFromHead(index) :
-      this.getFromTail(index);
+    if (!this.length || index < 1 || index > this.length) return null; // Return null if the list is empty of the index is out of range
+    else return index <= this.length / 2 // Depending on the index, we'll traverse the list forward or backward
+      ? this.getFromHead(index)
+      : this.getFromTail(index);
   }
 
-  getFromHead(index) {
-    // Start at the head of the list
-    let node = this.head;
-    // Traverse the list until the nth item
-    for (let i = 1; i < index; i++) node = node.next;
-    // Return the requested node
-    return node;
+  getFromHead(index) { // Get the node at the given index starting at the head and traversing it forward
+    let node = this.head; // Start at the head of the list
+    for (let i = 1; i < index; i++) node = node.next; // Traverse the list until the nth item
+    return node; // Return the requested node
   }
 
-  getFromTail(index) {
-    // Start at the tail of the list
-    let node = this.tail;
-    // Traverse the list until the nth item
-    for (let i = this.length; i > index; i--) node = node.prev;
-    // Return the requested node
-    return node;
+  getFromTail(index) { // Get the node at the given index starting at the tail and traversing it backward
+    let node = this.tail; // Start at the tail of the list
+    for (let i = this.length; i > index; i--) node = node.prev; // Traverse the list until the nth item
+    return node; // Return the requested node
   }
 
   set(index, val) { // Change the value of the node at the given index
-    const node = this.get(index);
-     // If the node doesn't exists (it's value is null), return null
-    if (!node) return null;
-    // Else assign the new value to the node
-    else node.val = val;
-    // Return the list with the modified node
-    return true;
+    const node = this.get(index); // Get the node at the given index
+    if (!node) return null; // If the node doesn't exist (it's value is null), return null
+    else node.val = val; // Else assign the new value to the node
+    return this; // Return the list with the modified node
   }
 
   insert(index, val) { // Add a node at a specific position (1-based)
-    // Index can't be lower than 1 or greater than length + 1
-    if (index < 1 || index > this.length + 1) return null;
-    // Create the new node to be added
-    const newNode = Node(val);
-    // If the index is 1, we should simply unshift the new node
-    if (index === 1) return this.unshift(val);
-    // Else if the index is the length of the list + 1, push the new node
-    if (index > this.length) return this.push(val);
+    if (index < 1 || index > this.length + 1) return null; // Index can't be lower than 1 or greater than length + 1
+    const newNode = Node(val); // Create the new node to be added
+    if (index === 1) return this.unshift(val); // If the index is 1, we should simply unshift the new node
+    if (index > this.length) return this.push(val); // Else if the index is the length of the list + 1, push the new node
     // If it shouldn't unshift it nor push it, we proceed to insert it
-    // Keep a reference to the node to be displaced forward
-    const nextNode = this.get(index);
-    // Keep a reference to the previous node
-    const prevNode = nextNode.prev;
-    // Point the previous node's next and next node's prev to the newNode
-    prevNode.next = nextNode.prev = newNode;
+    const nextNode = this.get(index); // Keep a reference to the node to be displaced forward
+    const prevNode = nextNode.prev; // Keep a reference to the previous node
+    prevNode.next = nextNode.prev = newNode; // Point the previous node's next and next node's prev to the newNode
     // Set pointers for new node
     newNode.prev = prevNode;
     newNode.next = nextNode;
-    // Increase list length
-    ++this.length;
-    // Return the list with the new inserted element
-    return this;
+    this.length++; // Add 1 to list length
+    return this; // Return the list with the new inserted element
   }
 
   remove(index) { // Remove and return the node at the specified index
-    // Index can't be lower than 1 or greater than length + 1    
-    if (index < 1 || index > this.length) return null;
-    // If the node is at the beginning of the list, shift it and return it
-    if (index === 1) return this.shift();
-    // If the node is at the end of the list, pop it and return it
-    if (index === this.length) return this.pop();
+    if (!this.length || index < 1 || index > this.length) return null; // Return null if the list is empty of the index is out of range
+    if (index === 1) return this.shift(); // If the node is at the beginning of the list, shift it and return it
+    if (index === this.length) return this.pop(); // If the node is at the end of the list, pop it and return it
     // Else, we proceed to remove it
-    // Get the node at the given index
-    const node = this.get(index);
-    // Set the previous node's next to the next node (skip the removed node)
-    node.prev.next = node.next;
-    // Set the next node's prev to the previous node (skip the removed node)
-    node.next.prev = node.prev;
-    // Remove node references to the prev and next ones
-    node.prev = node.next = null;
-    // Subtract 1 to the length of the list
-    --this.length;
-    // Return the removed node
-    return node;
+    const node = this.get(index); // Get the node at the given index
+    node.prev.next = node.next; // Set the previous node's next to the next node (skip the removed node)
+    node.next.prev = node.prev; // Set the next node's prev to the previous node (skip the removed node)
+    node.prev = node.next = null; // Remove node references to the prev and next ones
+    this.length--; // Subtract 1 to the length of the list
+    return node; // Return the removed node
   }
 }
---
+++ data-structures/singly-linked-list/SinglyLinkedList-commented.js
@@ -0,0 +1,106 @@
+// Implementation of a singly linked list and all of its methods, including reverse.
+
+// Node in the list, using a factory function instead of a class
+const Node = val => ({
+  val, // Value of the node
+  next: null, // Reference to the next node in the list
+});
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null; // Reference to the head (first node in the list)
+    this.tail = null; // Reference to the tail (last node in the list)
+    this.length = 0; // Length of the list
+  }
+
+  push(val) { // insert a new node with the given value at the end of the list
+    const node = Node(val); // Create a new node using with the received value
+    if (!this.head) this.head = this.tail = node; // if there is no head, set the head and tail to be the newly created node
+    else this.tail.next = this.tail = node; // Otherwise, set the next property on the tail and the tail property on the list to be the new node
+    this.length++; // Add 1 to list length
+    return this; // Return the list
+  }
+
+  pop() { // Remove and return the last element in the list
+    if (!this.length) return null; // If the list is empty, we return null
+    const prevTail = this.tail; // Keep a copy of the previous tail
+    let newTail = this.head; // Assign a new tail, initially to the previous head so we can traverse the list
+    while (newTail.next && newTail.next.next) newTail = newTail.next; // Traverse the list and find the 2nd to last element
+    newTail.next = null; // Erase the reference to the next element (the previous tail) from the new tail
+    this.tail = newTail; // Assign the new tail to the list
+    this.length--; // Subtract 1 to list's length
+    if (!this.length) this.head = this.tail = null; // If the list is empty, both the head and the tail should be null
+    return prevTail; // Return the value of the previous tail (the removed node)
+  }
+
+  shift() { // Remove and return the first element in the list
+    if (!this.length) return null; // If the list is empty, return null
+    const prevHead = this.head; // Save a reference to the current head
+    this.head = this.head.next; // Set the list head to the 2nd element (head.next) or null
+    prevHead.next = null; // Set the previous head next to null
+    this.length--; // Subtract 1 to list's length
+    if (!this.length) this.tail = null; // If the list is now empty, set the tail to null (head was already null)
+    return prevHead; // Return the previous head (the removed node)
+  }
+
+  unshift(val) { // insert a new node with the given value at the beginning of the list
+    const node = Node(val); // Create the new node
+    node.next = this.head; // Set its next prop to the current head of the list (will be null if list was empty)
+    this.head = node; // Set the new node as head of the list
+    if (!this.tail) this.tail = node; // If the list was empty (there was no tail), set new node as tail
+    this.length++; // Add 1 to list's length
+    return this; // Return a reference to the list
+  }
+
+  get(index) { // get the node at the given index (1-based)
+    if (!this.length || index < 1 || index > this.length) return null; // Return null if the list is empty of the index is out of range
+    let node = this.head; // Start at the head of the list
+    for (let i = 1; i < index; i++) node = node.next; // Traverse the list until the nth item
+    return node; // Return the requested node
+  }
+
+  set(index, val) { // Change the value of the node at the given index
+    const node = this.get(index); // Get the node at index
+    if (!node) return null; // Return null if there was no node at the given index
+    node.val = val; // Set the value of the node at index to val
+    return this; // Return a reference to the list
+  }
+
+  insert(index, val) { // Add a node at a specific position (1-based)
+    if (index < 1 || index > this.length + 1) return false; // Return null if the index is out of range
+    if (index === 1) return this.unshift(val); // Insert at head if index is 1 and return a reference to the list
+    if (index === this.length + 1) return this.push(val); // Push if index is at the end of the list and return a reference to the list
+    const node = Node(val); // Else create a new node
+    const prevNode = this.get(index - 1); // Get the previous node (index - 1)
+    node.next = prevNode.next; // Point the new node's next to the previous node's next (null if prevNode was the tail)
+    prevNode.next = node; // Point previous node's next to the new node
+    this.length++; // Add 1 to list's length
+    return this; // Return a reference to the list
+  }
+
+  remove(index) { // Remove and return a node at a specific position (1-based)
+    if (index < 1 || index > this.length) return null; // Return null if the index is out of range
+    if (index === 1) return this.shift(index); // Shift the head and return the node if necessary
+    if (index === this.length) return this.pop(index); // Pop the tail and return the node if necessary
+    const prevNode = this.get(index - 1); // Get the element before the index
+    const removedNode = prevNode.next; // Save a reference to the node to be removed
+    prevNode.next = removedNode.next; // Point prevNode.next to the next node
+    removedNode.next = null; // Isolate (remove the reference to next node)
+    this.length--;
+    return removedNode; // Return the removed node
+  }
+
+  reverse() { // Reverse the whole list
+    [this.head, this.tail] = [this.tail, this.head]; // Swap head and tail
+    let currNode = this.tail, // Keep a reference to the current tail (previous head)
+      nextNode = this.tail.next; // and the next node (of the previous head, current tail)
+    while (nextNode) { // Traverse the list and swap every reference while there's still a next node
+      const prevNode = currNode; // Keep a reference to the current node as the previous node
+      currNode = nextNode; // Replace the current node reference with a reference to the next node
+      nextNode = currNode.next; // Now that currNode points to the next node, nextNode has to point to currNode.next
+      currNode.next = prevNode; // Once we've got the appropriate references to currNode and nexNode, we can finally reverse the link
+    }
+    this.tail.next = null; // Set the list tail's next to null (as the tail was originally the head and its next pointed to the 2nd node)
+    return this;
+  }
+}

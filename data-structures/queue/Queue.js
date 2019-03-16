0124a13f5509a901295c2780702e4db594848547
JBeCast <jbcsound@gmail.com>
2019-03-16 22:25:11 +0100

    Minor changes to Queue data structure.

--- data-structures/queue/Queue.js
+++ data-structures/queue/Queue.js
@@ -1,33 +1,31 @@
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null;
-    }
-}
+const Node = val => ({
+  val,
+  next: null,
+});
 
 class Queue {
-    constructor() {
-        this.front = null;
-        this.rear = null;
-        this.size = 0;
-    }
+  constructor() {
+    this.front = null;
+    this.rear = null;
+    this.size = 0;
+  }
 
-    enqueue(val) {
-        const newElem = new Node(val);
-        if (!this.size) this.front = this.rear = newElem;
-        else {
-            this.rear.next = newElem;
-            this.rear = newElem;
-        }
-        return ++this.size;
+  enqueue(val) {
+    const newElem = Node(val);
+    if (!this.size) this.front = this.rear = newElem;
+    else {
+      this.rear.next = newElem;
+      this.rear = newElem;
     }
+    return ++this.size;
+  }
 
-    dequeue() {
-        if (!this.size) return null;
-        const poppedElem = this.front;
-        this.size--;
-        if (!this.size) this.front = this.rear = null;
-        else this.front = poppedElem.next;
-        return poppedElem.val;
-    }
+  dequeue() {
+    if (!this.size) return null;
+    const poppedElem = this.front;
+    --this.size;
+    if (!this.size) this.front = this.rear = null;
+    else this.front = poppedElem.next;
+    return poppedElem.val;
+  }
 }

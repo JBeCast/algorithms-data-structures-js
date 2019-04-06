454d12428d76643fb3fbc5e2d548e32d217ff7aa
Joaquín Bermúdez Castanheira <jbcsound@gmail.com>
2019-04-06 10:31:48 +0200

    Add tests for SinglyLinkedList shift and unshift methods.

--- data-structures/singly-linked-list/SinglyLinkedList.spec.js
+++ data-structures/singly-linked-list/SinglyLinkedList.spec.js
@@ -1,6 +1,6 @@
 describe('SinglyLinkedList', () => {
   const SinglyLinkedList = require('./SinglyLinkedList');
-  const _clone = require('lodash/clone');
+  const _clone = require('lodash/cloneDeep');
 
   const sampleList = new SinglyLinkedList();
   sampleList.push(1).push(2).push(3);
@@ -55,7 +55,7 @@ describe('SinglyLinkedList', () => {
   describe('Pop method', () => {
     it('should behave as expected', () => {
       const sll = _clone(sampleList);
-
+      
       expect(sll.length).toBe(3); // Initial length
       expect(sll.tail.val).toBe(3);
       let poppedNode = sll.pop();
@@ -81,5 +81,57 @@ describe('SinglyLinkedList', () => {
       expect(poppedNode).toBe(null);
     });
   });
+  
+  describe('Shift method', () => {
+    it('should behave as expected', () => {
+      const sll = _clone(sampleList);
 
+      expect(sll.length).toBe(3); // Initial length
+      expect(sll.tail.val).toBe(3);
+      let poppedNode = sll.shift();
+      expect(sll.length).toBe(2);
+      expect(sll.tail).not.toBe(poppedNode);
+      expect(poppedNode.val).toBe(1);
+      expect(poppedNode.next).toBe(null);
+      expect(sll.tail.val).toBe(3);
+      expect(sll.tail.next).toBe(null);
+      poppedNode = sll.shift();
+      expect(sll.length).toBe(1);
+      expect(poppedNode.val).toBe(2);
+      expect(sll.head).toBe(sll.tail); // Only one element remaining
+      expect(sll.tail.val).toBe(3);
+      poppedNode = sll.shift();
+      expect(poppedNode.val).toBe(3);
+      expect(sll.length).toBe(0);
+      expect(sll.head).toBe(null); // No elements remaining
+      expect(sll.tail).toBe(null);
+      poppedNode = sll.shift(); // Pop on an empty list
+      expect(poppedNode).toBe(null);
+      poppedNode = sll.shift(); // Once again
+      expect(poppedNode).toBe(null);
+    });
+  });
+
+  describe('Unshift method', () => {
+    it('should behave as expected', () => {
+      const sll = new SinglyLinkedList();
+
+      expect(sll.length).toBe(0); // Initial length
+      sll.unshift(1);
+      expect(sll.length).toBe(1); // Length should be increased after pushing
+      expect(sll.head).toBe(sll.tail); // Head and tail are the same object when only one node in the list
+      expect(typeof sll.head).toBe('object');
+      expect(sll.head.val).toBe(1);
+      expect(sll.head.next).toBe(null);
+
+      // Unshift method returns a reference to the list object, so unshift ops can be chained
+      sll.unshift(2).unshift(3).unshift(4).unshift(5);
+      expect(sll.length).toBe(5); // Length should be increased after unshifting
+      expect(sll.head.val).toBe(5); // Head value has been shifted every time
+      expect(sll.head.next.val).toBe(4);
+      expect(sll.head.next.next.val).toBe(3);
+      expect(sll.tail.val).toBe(1); // First head is now the tail
+      expect(sll.tail.next).toBe(null);
+    });
+  });  
 });
